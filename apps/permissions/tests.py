841cce68194ac6d7b4428582e07f89f98d06175d
Nagabrahmam Mantha <92820460+naga-mantha@users.noreply.github.com>
2025-08-13 18:35:46 -0400

    Fix instance binding in PermissionFormMixin

--- apps/permissions/tests.py
+++ apps/permissions/tests.py
@@ -1,3 +1,33 @@
+from django import forms
+from django.apps import apps as django_apps
+from django.db import models
 from django.test import TestCase
+from types import SimpleNamespace
 
-# Create your tests here.
+from apps.permissions.forms import PermissionFormMixin
+
+
+class DummyModel(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        app_label = "permissions"
+
+
+django_apps.register_model("permissions", DummyModel)
+
+
+class DummyForm(PermissionFormMixin, forms.ModelForm):
+    class Meta:
+        model = DummyModel
+        fields = ["name"]
+
+
+class PermissionFormMixinTest(TestCase):
+    def test_instance_binding(self):
+        instance = DummyModel(name="foo")
+        user = SimpleNamespace(is_superuser=True, is_staff=True, has_perm=lambda perm: True)
+        form = DummyForm(user=user, instance=instance)
+
+        self.assertIs(form.instance, instance)
+        self.assertEqual(form.initial["name"], "foo")
