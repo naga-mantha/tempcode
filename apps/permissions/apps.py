114d3a9f76e78795513b2e2acf2a3d534e672cf5
Nagabrahmam Mantha <92820460+naga-mantha@users.noreply.github.com>
2025-08-14 19:39:07 -0400

    Remove signal-based permission cache clearing

--- apps/permissions/apps.py
+++ apps/permissions/apps.py
@@ -1,12 +1,4 @@
 from django.apps import AppConfig
-from django.core.signals import request_finished
-
-from .checks import clear_perm_cache
-
-
-def _clear_perm_cache(**kwargs):
-    """Signal handler to clear the permission cache."""
-    clear_perm_cache()
 
 
 class PermissionsConfig(AppConfig):
@@ -15,6 +7,3 @@ class PermissionsConfig(AppConfig):
 
     def ready(self):
         import apps.permissions.signals
-        request_finished.connect(
-            _clear_perm_cache, dispatch_uid="apps.permissions.clear_perm_cache"
-        )
