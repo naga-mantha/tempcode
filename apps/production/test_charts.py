69ce7b67d3a8b27ffb23dd04c00b42ec53f4d333
Nagabrahmam Mantha <92820460+naga-mantha@users.noreply.github.com>
2025-08-27 14:01:06 -0400

    Add production order charts

--- apps/production/test_charts.py
+++ apps/production/test_charts.py
@@ -1,25 +1,53 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 import django
 
 django.setup()
 
-from apps.production.charts import SalesByMonthChart
+from apps.production.charts import (
+    ProductionOrdersByStatusChart,
+    ProductionOrdersPerItemBarChart,
+    ProductionOrdersPerItemLineChart,
+)
+from apps.common.models import ProductionOrder, Item
 
 
-class SalesByMonthChartTests(SimpleTestCase):
+class ProductionChartsTests(TestCase):
     def setUp(self):
-        self.chart = SalesByMonthChart()
+        self.status_chart = ProductionOrdersByStatusChart()
+        self.bar_chart = ProductionOrdersPerItemBarChart()
+        self.line_chart = ProductionOrdersPerItemLineChart()
 
-    def test_region_filter_has_fixed_choices(self):
-        schema = self.chart.get_filter_schema(None)
-        region_cfg = schema.get("region")
-        self.assertIsNotNone(region_cfg)
-        self.assertEqual(region_cfg.get("type"), "select")
-        self.assertEqual(
-            region_cfg.get("choices"),
-            [("all", "All"), ("na", "North America"), ("eu", "Europe")],
+        item1 = Item.objects.create(code="ITM1")
+        item2 = Item.objects.create(code="ITM2")
+
+        ProductionOrder.objects.create(
+            production_order="PO1", status="open", item=item1
         )
-        self.assertNotIn("choices_url", region_cfg)
-        opts = region_cfg.get("tom_select_options")
-        self.assertIsNotNone(opts)
-        self.assertFalse(opts.get("create"))
+        ProductionOrder.objects.create(
+            production_order="PO2", status="open", item=item1
+        )
+        ProductionOrder.objects.create(
+            production_order="PO3", status="closed", item=item2
+        )
+
+    def test_status_filter_schema_is_multiselect(self):
+        schema = self.status_chart.get_filter_schema(None)
+        cfg = schema.get("status")
+        self.assertIsNotNone(cfg)
+        self.assertEqual(cfg.get("type"), "multiselect")
+
+    def test_donut_chart_counts_by_status(self):
+        data = self.status_chart.get_chart_data(None, {})
+        self.assertEqual(data["labels"], ["closed", "open"])
+        self.assertEqual(data["values"], [1, 2])
+
+    def test_bar_chart_filters_by_status(self):
+        data = self.bar_chart.get_chart_data(None, {"status": ["open"]})
+        self.assertEqual(data["x"], ["ITM1"])
+        self.assertEqual(data["y"], [2])
+
+    def test_line_chart_returns_all_items(self):
+        data = self.line_chart.get_chart_data(None, {})
+        self.assertEqual(data["x"], ["ITM1", "ITM2"])
+        self.assertEqual(data["y"], [2, 1])
+
