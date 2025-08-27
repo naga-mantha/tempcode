37163e95bb3f5b702b7906ae6c3d3c4c5152d9ef
Nagabrahmam Mantha <92820460+naga-mantha@users.noreply.github.com>
2025-08-27 10:37:16 -0400

    Allow custom Tom Select options in filters

--- apps/common/templatetags/dict_extras.py
+++ apps/common/templatetags/dict_extras.py
@@ -1,4 +1,6 @@
 from django import template
+import json
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -8,3 +10,12 @@ def get_item(d, key):
         return d.get(key)
     except Exception:
         return None
+
+
+@register.filter
+def tojson(value):
+    """Serialize a Python object to JSON for embedding in templates."""
+    try:
+        return mark_safe(json.dumps(value))
+    except Exception:
+        return mark_safe("null")
