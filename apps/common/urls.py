b1ba96f0e9adc6a6aca88ae87438ffaaa42716ae
Naga <92820460+naga-mantha@users.noreply.github.com>
2025-10-19 18:45:43 -0400

    Clean up table and pivot links and expose items pivot

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .pivots import render_items_pivot
 from .tables import render_items_table
 from .views import home, roadmap_list, todo_list, todo_reorder
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('todos/reorder/', todo_reorder, name='todo_reorder'),
     path('roadmap/', roadmap_list, name='roadmap_list'),
     path('blocks/table/items', render_items_table, name='table_items'),
+    path('blocks/pivot/items', render_items_pivot, name='pivot_items'),
 ]
