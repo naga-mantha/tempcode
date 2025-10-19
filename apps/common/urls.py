a0a641f4e6ad6179e98ceec016fcb33fa7686448
Naga <92820460+naga-mantha@users.noreply.github.com>
2025-10-19 18:36:52 -0400

    Expose items table through common URLs

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import *
-from .tables import *
+
+from .tables import render_items_table
+from .views import home, roadmap_list, todo_list, todo_reorder
 
 urlpatterns = [
     path('', home, name='home'),
     path('todos/', todo_list, name='todo_list'),
     path('todos/reorder/', todo_reorder, name='todo_reorder'),
-    path('roadmap/', roadmap_list, name='roadmap_list'),
-
     path('roadmap/', roadmap_list, name='roadmap_list'),
     path('blocks/table/items', render_items_table, name='table_items'),
 ]
